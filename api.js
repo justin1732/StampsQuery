dd32cc33433c2abcdecf09cf6de6f14f0eec5ebe
Justin Pierce Tucker <57789046+justin1732@users.noreply.github.com>
2021-05-16 22:57:27 -0500

    Pushing changes and jquery

--- api.js
+++ api.js
@@ -1,10 +1,20 @@
+// $(document).ready(function (){
 var myHeaders = new Headers();
 myHeaders.append("Host", "api.shipengine.com");
 myHeaders.append("API-Key", "TEST_J6h+wzVuOOQlnxi4H6VwhZoBWolFjnM61n6J3K1bTcY");
 myHeaders.append("Content-Type", "application/json");
 myHeaders.append("Access-Control-Allow-Origin", "*");
+var address = address;
+var city = city;
+var zip = zip;
+var state= state;
 
-var raw = JSON.stringify([{"address_line1":"525 S Winchester Blvd","city_locality":"San Jose","state_province":"CA","postal_code":"95128","country_code":"US"}]);
+var raw = JSON.stringify([{
+    "address_line1":"525 S Winchester Blvd",
+    "city_locality":"San Jose",
+    "state_province":"CA",
+    "postal_code":"95128",
+    "country_code":"US"}]);
 
 var requestOptions = {
   method: 'POST',
@@ -17,3 +27,9 @@ fetch("https://api.shipengine.com/v1/addresses/validate", requestOptions)
   .then(response => response.text())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
+$("#validationButton").on("click", function(event){
+    event.preventDefault();
+    
+    
+})
+// })
